161fbe8a9730cd536f2802f1d4cdc2888b1cfc12
Pierluca Borsò <pierluca.borso@epfl.ch>
2021-05-18 17:23:17 +0200

    Add slides for debugging

--- code/prom/main.go
+++ code/prom/main.go
@@ -1,34 +1,88 @@
 package main
 
 import (
-        "net/http"
-        "time"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
-        "github.com/prometheus/client_golang/prometheus"
-        "github.com/prometheus/client_golang/prometheus/promauto"
-        "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+
+	perfHist = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "myapp_endpoint_sort_ms",
+		Help: "The response time histogram for the sort endpoint",
+	})
 )
 
 func recordMetrics() {
-        go func() {
-                for {
-                        opsProcessed.Inc()
-                        time.Sleep(2 * time.Second)
-                }
-        }()
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
 }
 
-var (
-        opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-                Name: "myapp_processed_ops_total",
-                Help: "The total number of processed events",
-        })
-)
+func track(h prometheus.Histogram) (prometheus.Histogram, time.Time) {
+	return h, time.Now()
+}
+
+func duration(h prometheus.Histogram, start time.Time) {
+	ms := time.Since(start).Nanoseconds()
+	h.Observe(float64(ms))
+}
+
+// -------------------------------------------------
+
+func swapPlaces(array []int, j int) {
+	array[j], array[j+1] = array[j+1], array[j]
+}
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				swapPlaces(arr, j)
+			}
+		}
+	}
+}
+
+// -------------------------------------------------
+
+func parseIntArray(s string) []int {
+	arr := strings.Split(s, ",")
+	n := len(arr)
+	nums := make([]int, n)
+	for i, v := range arr {
+		nums[i], _ = strconv.Atoi(v)
+	}
+	return nums
+}
+
+func sortHandler(w http.ResponseWriter, r *http.Request) {
+	defer duration(track(perfHist))
+
+	v := r.URL.Query().Get("values")
+	nums := parseIntArray(v)
+	bubbleSort(nums)
+	fmt.Fprintf(w, "%v", nums)
+}
 
 func main() {
-        recordMetrics()
+	recordMetrics()
 
-        http.Handle("/metrics", promhttp.Handler())
-        http.ListenAndServe(":2112", nil)
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/sort", sortHandler)
+	http.ListenAndServe(":2112", nil)
 }
-
